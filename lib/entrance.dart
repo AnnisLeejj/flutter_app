f59d9cdff8cfb396271499b5aafdbc359550d6c0
DESKTOP-9B4UMLR\Annis <l536510961@126.com>
2019-07-22 17:33:34 +0800

    Flutter中文网 -开始

---
+++ lib/entrance.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+import 'myLib.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();  // 删除此行
+
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+          //child: new Text(wordPair.asPascalCase),
+          child: new RandomWords(),
+        ),
+      ),
+    );
+  }
+}
